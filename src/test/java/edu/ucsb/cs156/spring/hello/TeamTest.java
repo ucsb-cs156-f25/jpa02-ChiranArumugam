e6333b59ee794bd6b53b5929643ce996b7fce826
Chiran Arumugam <carumugam@csil.cs.ucsb.edu>
2025-10-07 18:17:11 -0700

    ca - added team tests for equals and hashCode

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -2,6 +2,8 @@ package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.beans.Transient;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,7 +21,87 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_compares_same_class() {
+        Team team1 = new Team("team1");
+        team1.addMember("Ryan");
+        team1.addMember("Chiran");
+
+        Team team2 = null;
+
+        assertEquals(false, team1.equals(team2));
+    }
+
+    @Test
+    public void equals_compares_same_object() {
+        Team team1 = new Team("team1");
+        team1.addMember("Ryan");
+        team1.addMember("Chiran");
+
+        assertEquals(true, team1.equals(team1));
+    }
+
+    @Test
+    public void equals_returns_correct_equivalence() {
+        Team team1 = new Team("team1");
+        team1.addMember("Ryan");
+        team1.addMember("Chiran");
+
+        Team team2 = new Team("team1");
+        team2.addMember("Ryan");
+        team2.addMember("Chiran");
+
+        assertEquals(true, team1.equals(team2));
+    }
+
+    @Test
+    public void equals_returns_correct_members() {
+        Team team1 = new Team("team1");
+        team1.addMember("Ryan");
+        team1.addMember("Chiran");
+
+        Team team2 = new Team("team1");
+        team2.addMember("Dave");
+        team2.addMember("Anthony");
+
+        assertEquals(false, team1.equals(team2));
+    }
+
+    @Test
+    public void equals_returns_correct_inequivalence() {
+        Team team1 = new Team("team1");
+        team1.addMember("Ryan");
+        team1.addMember("Chiran");
+
+        Team team2 = new Team("team2");
+        team2.addMember("Dave");
+        team2.addMember("Anthony");
+
+        assertEquals(false, team1.equals(team2));
+    }
+
+    @Test
+    public void hashCode_returns_correct_hash() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("bar");
+        Team t2 = new Team();
+        t2.setName("foo");
+        t2.addMember("bar");
+        assertEquals(t1.hashCode(), t2.hashCode());
+    }
+
+    @Test
+    public void hashCode_returns_correct_hash_mutation_equivalent() {
+        int result = team.hashCode();
+        int expectedResult = -1226298695;
+        assertEquals(expectedResult, result);
+    }
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
